c58c99d4efc09f5b2dbadb877e76f04c12823070
Lu Nguyen <lu@redweb.dk>
2020-10-09 17:13:13 +0700

    real-favicon-webpack-plugin

---
+++ library/real-favicon-webpack-plugin/README.md
@@ -0,0 +1,46 @@
+[![npm version](https://badge.fury.io/js/real-favicon-webpack-plugin.svg)](https://badge.fury.io/js/real-favicon-webpack-plugin)
+
+# real-favicon-webpack-plugin
+
+Generate favicons using [RealFaviconGenerator](https://github.com/RealFaviconGenerator) as part of your Webpack build process.
+
+
+## Install
+
+```
+npm i --save-dev real-favicon-webpack-plugin
+```
+
+
+## Usage
+
+In your Webpack config:
+
+**webpack.config.js**
+
+```js
+const RealFaviconPlugin = require('real-favicon-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new RealFaviconPlugin({
+        favicon: path.resolve(__dirname, "./src/favicon/favicon.png"),
+        faviconJson: path.resolve(__dirname, "./src/favicon/favicon.json"),
+        publicPath: "/publicPath/favicon",
+        outputPath: "/outputPath/favicon",
+    })
+  ]
+}
+```
+
+The options are:
+
+* `favicon` (required): the path to a favicon icon
+* `faviconJson` (required): the path to a JSON configuration file from the [RFG website](https://realfavicongenerator.net)
+* `publicPath` (required): the path where the plugin will output the public path
+* `outputPath` (required): the path where the plugin will output the favicon files generated from your config
+
+
+### Generated files
+
+The plugin will generate files based on your configuration file. Generally these will include one or more favicon images, and possibly some browser- or device-specific files such as `browserconfig.xml` for IE11.
---
+++ library/real-favicon-webpack-plugin/index.js
@@ -0,0 +1,67 @@
+const rfg = require("rfg-api").init();
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const pluginName = "RealFaviconPlugin";
+
+class RealFaviconPlugin {
+  // Do some setup and check we were given the correct options
+  constructor(options) {
+    const { favicon, faviconJson, publicPath, outputPath } = options;
+    if (!favicon) throw new Error(`${pluginName}: Missing favicon option`);
+    if (!faviconJson)
+      throw new Error(`${pluginName}: Missing faviconJson option`);
+    if (!publicPath)
+      throw new Error(`${pluginName}: Missing publicPath option`);
+    if (!outputPath)
+      throw new Error(`${pluginName}: Missing outputPath option`);
+
+    this.options = options;
+  }
+
+  // Generate and write favicon images
+  generateFavicons(cb) {
+    rfg.generateFavicon(this.request, this.options.outputPath, cb);
+  }
+
+  apply(compiler) {
+    // Hook into the webpack compilation
+    // to start the favicon generation
+    compiler.hooks.make.tapPromise(pluginName, async (compilation) => {
+      const jsonPath = this.options.faviconJson;
+      const json = require(jsonPath);
+
+      const opts = {
+        apiKey: "114204dabff7639f8a4c1a567e98b57d2cf8e76e",
+        masterPicture: this.options.favicon,
+        iconsPath: this.options.publicPath,
+        design: json.design,
+        settings: json.settings,
+        versioning: json.versioning,
+      };
+
+      this.request = rfg.createRequest(opts);
+
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        pluginName,
+        (data, cb) => {
+          if (data.plugin.options.realfavicons === true) {
+            this.generateFavicons((err, res) => {
+              if (err) return cb(err);
+
+              data.html = data.html.replace(
+                "</head>",
+                res.favicon.html_code + "</head>"
+              );
+
+              cb(null, data);
+            });
+          } else {
+            cb(null, data);
+          }
+        }
+      );
+    });
+  }
+}
+
+module.exports = RealFaviconPlugin;
